4458bf839e3a2573d2e0974744260220089cd821
Martin <erdei.martin.2021e@ms.gdszeged.hu>
2023-03-16 09:29:00 +0100

    V1.0

---
+++ python_py43.py
@@ -0,0 +1,16 @@
+import random
+
+lista=[]
+
+
+for _ in range(5):
+    szam=random.randint(1,10)
+    lista.append(szam)
+    
+összeg=0
+    
+for szam in lista:
+    összeg+=szam
+    
+print(lista)
+print(összeg)
